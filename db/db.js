43394aef3dd3aea097f55802f3196b851991611c
Dmytriyev <dmytriyevapple@gmail.com>
2025-12-08 11:39:38 +0100

    COM_3

--- db/db.js
+++ db/db.js
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 
 export default async function db() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
+    const MONGODB_URI = process.env.MONGODB_URI;
+    await mongoose.connect(MONGODB_URI);
+    console.log("Connected to MongoDB:", MONGODB_URI);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
     process.exit(1);
--- server.js
+++ server.js
@@ -3,10 +3,10 @@ import express from "express";
 import mongoose from "mongoose";
 import db from "./db/db.js";
 import cors from "cors";
-import postRoutes from './routes/post.route.js';
+import postRoutes from "./routes/post.route.js";
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -20,6 +20,5 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-
 // routes----------
-app.use('/post', postRoutes);
+app.use("/post", postRoutes);
